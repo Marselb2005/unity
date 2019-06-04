3d6ff712eb1a274adc4b1d1a44449b48ff770673
Marselb2005 <33658297+Marselb2005@users.noreply.github.com>
2019-06-04 15:40:17 +0900

    Add files via upload

---
+++ CameraController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.AI;
+public class CameraController : MonoBehaviour
+{	
+	public GameObject bullet;
+	GameObject bulletClone;
+	Rigidbody rbClone; 
+
+    public Text hpText;
+    int HP;
+
+	Rigidbody rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        HP = 100;
+    }
+
+    void FixedUpdate()
+    {
+    	//Движение
+        float moveVertical = Input.GetAxis("Vertical");
+    	rb.AddForce(transform.forward * moveVertical * 50f);
+    
+    	//Повороты
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        transform.Rotate(0f,moveHorizontal * 10f,0f);
+
+        //Пули
+        if(Input.GetKey("space"))
+        {
+            bulletClone = Instantiate(bullet,new Vector3(transform.position.x, transform.position.y, transform.position.z), Quaternion.identity);
+            rbClone = bulletClone.GetComponent<Rigidbody>();
+            rbClone.AddForce(transform.forward * 600f);
+
+        }
+    }
+    void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.tag == "Enemy")
+        {
+            HP = HP - 20;
+            hpText.text = "HP: " + HP;
+            if(HP <= 0)
+            {
+                SceneManager.LoadScene(1);
+            }
+        }     
+    }
+}
---
+++ Enemy.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Enemy : MonoBehaviour
+{
+	public Transform player;
+	public NavMeshAgent agent;
+
+    public Text ScoreText;
+    static int Score = 0;
+
+    void Update()
+    {
+        agent.SetDestination(player.transform.position);
+    }
+    //Столкновение врага и пули
+   void OnCollisionEnter(Collision lol){
+        if(lol.gameObject.tag == "Bullet")
+        {
+            Destroy(gameObject);
+            Score = Score + 1;
+            ScoreText.text = "Score:" + Score;
+            if(Score >= 6)
+            {
+                SceneManager.LoadScene(0);
+                Score = 0;
+            }
+        }
+    }
+}
